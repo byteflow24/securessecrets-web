8cb5aa93525c0cdd67941ab58e4372726959cb35
admin <srarfi75@gmail.com>
2024-12-13 13:56:32 +0300

    edits

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,3 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VV6NCJ0407');
+        
 window.addEventListener('DOMContentLoaded', () => {
     initializeSearchForm();
     initializeSecretLinks();

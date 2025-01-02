a5179702fac33eab887395e33cc0a8215b4ae611
admin <srarfi75@gmail.com>
2025-01-02 13:06:40 +0300

    uploads

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads'
+    if os.getenv('FLASK_ENV') == 'development':
+        UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')  # Local directory
+    else:
+        UPLOAD_FOLDER = '/var/data/uploads'  # Render persistent disk
     MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500 MB limit
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=15)
 

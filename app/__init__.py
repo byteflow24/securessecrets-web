6dd1a8ea48915bd2f5f7b185f25a3183ead10b05
admin <srarfi75@gmail.com>
2025-01-07 13:22:26 +0300

    replaced

--- app/__init__.py
+++ app/__init__.py
@@ -5,47 +5,12 @@ from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_compress import Compress
-from flask_talisman import Talisman
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 login_manager = LoginManager()
 bootstrap = Bootstrap()
 migrate = Migrate()
-
-csp = {
-    'default-src': ["'self'"],
-    'script-src': [
-        "'self'",
-        "https://cdn.jsdelivr.net",
-        "https://code.jquery.com",
-        "https://www.googletagmanager.com",
-        "https://use.fontawesome.com",
-        "https://cdnjs.cloudflare.com",
-    ],
-    'style-src': [
-        "'self'",
-        "https://cdn.jsdelivr.net",
-        "https://fonts.googleapis.com",
-    ],
-    'font-src': [
-        "'self'",
-        "https://cdn.jsdelivr.net",
-        "https://fonts.gstatic.com",
-    ],
-    'img-src': [
-        "'self'",
-        "https://pagead2.googlesyndication.com",
-    ],
-    'connect-src': [
-        "'self'",
-        "https://ep1.adtrafficquality.google",
-    ],
-    'frame-src': [
-        "'self'",
-        "https://googleads.g.doubleclick.net",
-    ],
-}
 
 def create_app():
     app = Flask(__name__)
@@ -64,7 +29,6 @@ def create_app():
     bootstrap.init_app(app)
     Compress(app)
     CSRFProtect(app)
-    Talisman(app, content_security_policy=csp)
 
     # Import inside function to avoid circular import
     with app.app_context():
